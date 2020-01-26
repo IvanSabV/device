68133fcc4b20e64059ec519353e9b0214d4b2569
IvanSabV <60038980+IvanSabV@users.noreply.github.com>
2020-01-27 02:48:28 +0400

    slider + write us + hover button

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function view(n) {
+    style = document.getElementById(n).style;
+    style.display = (style.display == 'flex') ? 'none' : 'flex';
+}
+
+
+var modal = document.getElementById('write-us');
+
+var btn = document.getElementById("write-but");
+
+var span = document.getElementsByClassName("imgclose")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
