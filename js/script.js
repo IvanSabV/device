a0d70230a96689665a1544b4d100830a16ebaa04
IvanSabV <60038980+IvanSabV@users.noreply.github.com>
2020-01-28 01:10:06 +0400

    index +

--- js/script.js
+++ js/script.js
@@ -48,3 +48,52 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+var slideIndex = 1;
+showDeli(slideIndex);
+
+// Thumbnail image controls
+function currentDeli(n) {
+  showDeli(slideIndex = n);
+}
+
+function showDeli(n) {
+    var i;
+    var slides = document.getElementsByClassName("deli");
+    var dots = document.getElementsByClassName("button");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "flex";
+    dots[slideIndex-1].className += " active";
+  }
+
+  function search(n) {
+    style = document.getElementById(n).style;
+    style.display = (style.display == 'initial') ? 'none' : 'initial';
+}
+
+var modal = document.getElementById('map-modal');
+
+var btn = document.getElementById("map-btn");
+
+var span = document.getElementsByClassName("imgclose")[0];
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
